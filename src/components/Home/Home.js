8b162b4e029986c39c4cd305d718c9886faf03ac
Han <u7618352@anu.edu.au>
2024-07-16 12:54:41 +1000

    modify the file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,29 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useRef } from "react";
+import { Container } from "react-bootstrap";
 import Particle from "../Particle";
 import backgroundVideo from "../../Assets/background.mp4"
 
 
 function Home() {
-  return (
-    <section>
-        <Container fluid className="home-section" id="home">
-            <video autoPlay loop muted id="video-background">
-                <source src={backgroundVideo} type="video/mp4"/>
-                Your browser does not support the video tag.
-            </video>
-            <Particle/>
-        </Container>
-    </section>
-  );
+    const videoRef = useRef(null);
+
+    return (
+        <section>
+            <Container fluid className="home-section" id="home">
+                <video
+                    ref={videoRef}
+                    autoPlay
+                    loop
+                    muted
+                    id="video-background"
+                >
+                    <source src={backgroundVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <Particle />
+            </Container>
+        </section>
+    );
 }
 
 export default Home;
